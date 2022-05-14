09f9213733db06aadd0859ce2ebe5328cf86434f
Тимур Хасанов <tim.hasanov24@gmail.com>
2022-05-14 20:15:57 +0300

    updated the CoolNumbers method

--- src/main/java/practice/CoolNumbers.java
+++ src/main/java/practice/CoolNumbers.java
@@ -1,6 +1,7 @@
 package practice;
 
-import java.util.Collections;
+
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.TreeSet;
@@ -8,8 +9,29 @@ import java.util.TreeSet;
 public class CoolNumbers {
 
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        List<String> list = new ArrayList<>();
+        for (int region1 = 0; region1 < 10; region1++) {
+            for (int region2 = 0; region2 < 199; region2++) {
+                String region = Integer.toString(region2);
+                if (region1 < 10) {
+                    region = "0" + region;
+                }
+                for (String s1 : list) {
+                    for (String s2 : list) {
+                        for (String s3 : list) {
+                            String number = String.format("%s%d%d%d%s%s%s", s1, region1, region1, region1, s2, s3, region);
+                            list.add(number);
+                        }
+                    }
+                }
+
+
+            }
+        }
+
+        return list;
     }
+
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
         return false;
