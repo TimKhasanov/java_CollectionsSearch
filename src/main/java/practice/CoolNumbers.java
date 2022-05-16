358c5560194c1a9e73def91d42a89f9afcd5b0a4
Тимур Хасанов <tim.hasanov24@gmail.com>
2022-05-16 13:04:47 +0300

    the task was completed in the CoolNumbers and maim method

--- src/main/java/practice/CoolNumbers.java
+++ src/main/java/practice/CoolNumbers.java
@@ -5,44 +5,42 @@ import java.util.*;
 public class CoolNumbers {
 
     public static List<String> generateCoolNumbers() {
+        String[] numbers = {"А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "У", "Х"};
         List<String> list = new ArrayList<>();
         for (int region1 = 0; region1 < 10; region1++) {
             for (int region2 = 0; region2 < 199; region2++) {
                 String region = Integer.toString(region2);
-                if (region1 < 10) {
+                if (region2 < 10) {
                     region = "0" + region;
                 }
-                for (String s1 : list) {
-                    for (String s2 : list) {
-                        for (String s3 : list) {
+                for (String s1 : numbers) {
+                    for (String s2 : numbers) {
+                        for (String s3 : numbers) {
                             String number = String.format("%s%d%d%d%s%s%s", s1, region1, region1, region1, s2, s3, region);
                             list.add(number);
                         }
                     }
                 }
-
-
             }
         }
-
         return list;
-
     }
 
+
     public static boolean bruteForceSearchInList(List<String> list, String number) {
-        return false;
+        return list.contains(number);
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+        return Collections.binarySearch(sortedList, number) >= 0;
     }
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+        return hashSet.contains(number);
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+        return treeSet.contains(number);
     }
 
 }
--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,14 +1,54 @@
 package practice;
 
+import java.util.*;
+
 public class Main {
-    /*
-    TODO:
-     - реализовать методы класса CoolNumbers
-     - посчитать время поиска введимого номера в консоль в каждой из структуры данных
-     - проанализоровать полученные данные
-     */
+    private static String number = "Р444РР100";
+    private static List list = CoolNumbers.generateCoolNumbers();
 
     public static void main(String[] args) {
+        list();
+        arrayList();
+        hasSet();
+        treeSet();
+    }
+
+    static void list() {
+        long time1 = System.nanoTime();
+        String search = CoolNumbers.binarySearchInList(list, number) ? "Номер найден" : "Номер не найден";
+        long time2 = System.nanoTime();
+        String time = Long.toString(time2 - time1);
+        System.out.println("Поиск перебором: " + search + ", поиск занял: " + time);
+    }
+
+    static void arrayList() {
+
+        List<String> sortingNumbers = new ArrayList<>(list);
+        Collections.sort(sortingNumbers);
+        long time1 = System.nanoTime();
+        String search1 = CoolNumbers.binarySearchInList(sortingNumbers, number) ? "Номер найден" : "Номер не найден";
+        long time2 = System.nanoTime();
+        String time = Long.toString(time2 - time1);
+        System.out.println("Бинарный поиск: " + search1 + ", поиск занял: " + time);
+    }
+
+    static void hasSet() {
+        HashSet<String> hashSet = new HashSet<>(list);
+        long time1 = System.nanoTime();
+        String search2 = CoolNumbers.searchInHashSet(hashSet, number) ? "Номер найден" : "Номер не найден";
+        long time2 = System.nanoTime();
+        String time = Long.toString(time2 - time1);
+        System.out.println("Поиск в HasSet: " + search2 + ", поиск занял: " + time);
+
+    }
+
+    static void treeSet() {
+        TreeSet<String> treeSet = new TreeSet<>(list);
+        long time1 = System.nanoTime();
+        String search3 = CoolNumbers.searchInTreeSet(treeSet, number) ? "Номер найден" : "Номер не найден";
+        long time2 = System.nanoTime();
+        String time = Long.toString(time2 - time1);
+        System.out.println("Поиск в TreeSet: " + search3 + ", поиск занял: " + time);
 
     }
 }
